15dde356777a435ce1c8892cc48eb35257e0f145
MatheusDLins <matheus934qwer@gmail.com>
2025-06-25 14:00:31 -0300

    feat: operadores aritmeticos

---
+++ Santander 2025 - Back-End com Python/Operadores e Manipulação de String com Python/operadores_aritmeticos.py
@@ -0,0 +1,10 @@
+produto_1 = 20
+produto_2 = 10
+
+print(f"20 + 10 = {produto_1 + produto_2} (Adição)")
+print(f"20 - 10 = {produto_1 - produto_2} (Subtração)")
+print(f"20 / 10 = {produto_1 / produto_2} (Divisão)")
+print(f"20 // 10 = {produto_1 // produto_2} (Divisão inteira)")
+print(f"20 * 10 = {produto_1 * produto_2} (Multiplicação)")
+print(f"20 % 10 = {produto_1 % produto_2} (Módulo / Resto da divisão)")
+print(f"20 ** 10 = {produto_1 ** produto_2} (Exponenciação)")
