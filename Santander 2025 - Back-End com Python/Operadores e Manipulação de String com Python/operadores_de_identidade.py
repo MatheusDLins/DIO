14d7ff5c28544231185107175f19658c52a78a6a
MatheusDLins <matheus934qwer@gmail.com>
2025-06-25 14:28:55 -0300

    feat: operadores de identidade

---
+++ Santander 2025 - Back-End com Python/Operadores e Manipulação de String com Python/operadores_de_identidade.py
@@ -0,0 +1,9 @@
+curso = "Curso de Python"
+nome_curso = curso
+saldo, limite = 200, 200
+
+print(f"curso is nome_curso ? {curso is nome_curso}")
+
+print(f"curso is not nome_curso ? {curso is not nome_curso}")
+
+print(f"saldo is limite ? {saldo is limite}")
