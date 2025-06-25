3d04493e43c8d7a0bf074b2474f1825a7429f38a
MatheusDLins <matheus934qwer@gmail.com>
2025-06-25 14:10:09 -0300

    feat: operadores de comparação

---
+++ Santander 2025 - Back-End com Python/Operadores e Manipulação de String com Python/operadores_de_comparacao.py
@@ -0,0 +1,12 @@
+saldo = 200
+saque = 200
+
+print(f"Saldo: {saldo}")
+print(f"Saque: {saque}")
+
+print(f"Saldo == Saque? {saldo == saque} (Igual)")
+print(f"Saldo != Saque? {saldo != saque} (Diferente)")
+print(f"Saldo > Saque?  {saldo > saque} (maior que)")
+print(f"Saldo >= Saque? {saldo >= saque} (maior ou igual)")
+print(f"Saldo < Saque?  {saldo < saque} (menor que)")
+print(f"Saldo <= Saque? {saldo <= saque} (menor ou igual)")
