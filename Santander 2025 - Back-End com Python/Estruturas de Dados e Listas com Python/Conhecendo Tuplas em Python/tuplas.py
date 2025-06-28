353e568b09141eb2f7191a87aadf8629e7e0897d
MatheusDLins <matheus934qwer@gmail.com>
2025-06-28 13:28:30 -0300

    feat: tuplas

---
+++ Santander 2025 - Back-End com Python/Estruturas de Dados e Listas com Python/Conhecendo Tuplas em Python/tuplas.py
@@ -0,0 +1,17 @@
+frutas = ("laranja", "maca", "uva")
+
+letras = tuple("python")
+
+numeros = tuple([1, 2, 3, 4])
+
+pais = ("Brasil")
+
+frutas = ("maça", "laranja", "maca", "uva", "laranja")
+frutas[0]
+frutas[2]
+
+frutas.count("laranja") # 2
+
+frutas.index("maca") # 2
+
+len(frutas) # 5
