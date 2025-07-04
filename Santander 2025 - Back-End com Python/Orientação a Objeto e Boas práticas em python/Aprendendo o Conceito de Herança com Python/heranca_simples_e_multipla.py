13e62b02888ea13cf9ecde9bc6d7d0c2bdcb79c6
MatheusDLins <matheus934qwer@gmail.com>
2025-07-04 11:39:15 -0300

    feat: herança simples e múltipla

---
+++ Santander 2025 - Back-End com Python/Orientação a Objeto e Boas práticas em python/Aprendendo o Conceito de Herança com Python/heranca_simples_e_multipla.py
@@ -0,0 +1,21 @@
+#Herança simples
+
+#Quando uma classe filha herda apenas uma classe pai, ela é chamada de herança simples.
+class A:
+    pass
+
+class B(A):
+    pass
+
+
+#Herança múltipla
+
+#Quando uma classe filha herda de várias classes pai, ela é chamada de herança múltipla.
+class A:
+    pass
+
+class B:
+    pass
+
+class C(A, B):
+    pass
